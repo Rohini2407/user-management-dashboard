730a2dd3f3c2cc2f760d895adad08876dc0cfd89
Rohini Dawange <127883630+Rohini2407@users.noreply.github.com>
2025-02-01 15:07:42 +0500

    Update Navbar.js

--- user-management-dashboard/src/components/Navbar.js
+++ user-management-dashboard/src/components/Navbar.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const Navbar = () => (
   <nav className="navbar navbar-dark bg-dark p-3">
-    <Link to="/" className="navbar-brand">User Management</Link>
+    <Link to="#" className="navbar-brand">User Management</Link>
     <Link to="/add" className="btn btn-primary">Add User</Link>
   </nav>
 );
